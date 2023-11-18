d4ffc7d4f676cef7c1167df21351e1a110b4d1c9
deepa548 <dk@my.centennialcollege.ca>
2023-11-17 22:34:44 -0500

    UI changed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(125, 155);
+            button1.Name = "button1";
+            button1.Size = new Size(244, 122);
+            button1.TabIndex = 0;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(446, 155);
+            button2.Name = "button2";
+            button2.Size = new Size(244, 122);
+            button2.TabIndex = 1;
+            button2.Text = "Search Recipes";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(231, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(298, 77);
+            label1.TabIndex = 2;
+            label1.Text = "Home Page";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -42,5 +77,9 @@
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
     }
 }
